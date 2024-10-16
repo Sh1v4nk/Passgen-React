742f92c17659e798c155b3b7f2514a3ba702f2d4
Sh1v4nk <shivankpandey113@gmail.com>
2024-10-17 00:30:25 +0530

    Add essential ui components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { Button } from "./ui/button";
+export { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+export { Checkbox } from "./ui/checkbox";
+export { Input } from "./ui/input";
+export { Label } from "./ui/label";
+export { Slider } from "./ui/slider";
